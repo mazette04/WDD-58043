e01431a53a96169af6858026102260908b2bfe0e
mazette04 <89300171+mazette04@users.noreply.github.com>
2022-09-30 09:19:17 +0800

    Add files via upload

--- demo1.php
+++ demo1.php
@@ -3,6 +3,8 @@
 <body bgcolor = "blue"
 <p>This HTML will get delivered as is</p>
 <?php echo "<p>But this code is interpreted by PHP and turned into HTML</p>";
+	$j = 4;
+	$k = 4;
 	
 	$num1 = 6;
 	$num2 = 2;
@@ -15,7 +17,23 @@
 	echo"<br>";
 	echo "6+2= $sum";
 	echo"<br>";
-	echo "My Name is".$username;
+	echo "My Name is  ".$username;
+	echo"<br>";
+	$pi = 3.1416;
+	$radius = 5;
+	echo $pi*($radius*$radius);
+	echo "<br>";
+	echo $j;
+	echo "<br>";
+	echo $k;
+	echo "<br>";
+	if($j===$k)
+	{
+			echo "Identical in terms of data type";
+	}
+	else{
+			echo "They are not indentical";
+	}
 //This is the snippet for php code 
 ?>
 
